64292965b10bb8fc3188a3a3420dbfd189fd84ad
Mosai57 <mosai57@gmail.com>
2013-11-05 15:51:47 -0800

    modified line counter

--- fileRandomizer.py
+++ fileRandomizer.py
@@ -3,7 +3,7 @@ f = open('<file path here>', 'r')
 output = f.readlines()
 with open('<file path here>') as fin:
     lines = sum(1 for line in fin)
-i = randint(0,lines)
+i = randint(0,lines-1)
 print output[i]
 print lines
 
