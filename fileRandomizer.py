956bfbf28530406a62d8bdf7ae7d3ceba435b75a
Mosai57 <mosai57@gmail.com>
2013-11-05 15:44:58 -0800

    Modified the main python program to work universally and get the file length

---
+++ fileRandomizer.py
@@ -0,0 +1,12 @@
+from random import randint
+f = open('<file path here>', 'r')
+output = f.readlines()
+with open('<file path here>') as fin:
+    lines = sum(1 for line in fin)
+i = randint(0,lines)
+print output[i]
+print lines
+
+##Optional part; Writes the output string to a new file;
+f = open('<file path here>', 'w')
+f.write(output[i])
